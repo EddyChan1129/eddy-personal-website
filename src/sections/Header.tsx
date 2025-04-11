4b91dbe74f49c3df536d7b2255742c893c46ccf9
EddyChan1129 <22026266d@connect.polyu.hk>
2025-04-10 22:28:22 -0400

    Implemented Header, Hero UI

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+import logo from "@/assets/icons/bars.svg";
 import { FC } from "react";
+import { Button } from "@/components/Button";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -25,7 +28,26 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed top-0 left-0 right-0 backdrop-blur-md">
+      <nav className="container max-w-full">
+        <div className="flex justify-between items-center h-20">
+          <div>
+            <a href=""></a>
+            <span className="text-xl font-bold uppercase">Eddy&nbsp; CHAN</span>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
+              <Image src={logo} alt="logo" />
+            </div>
+            <Button
+              variant="primary"
+              className="hidden md:inline-flex">Contact Me</Button>
+          </div>
+        </div>
+      </nav>
+
+    </header>)
 };
 
 export default Header;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,39 @@
 import { FC } from "react";
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import heroImage from "@/assets/images/hero-image.jpg";
+import downImage from "@/assets/icons/chevron-double-down.svg";
+import { Button } from "@/components/Button";
+import Image from "next/image";
 
 const Hero: FC = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div className="md:grid md:grid-cols-12 md:h-screen items-stretch">
+        <div className="md:col-span-7 flex flex-col justify-center">
+          <div className="container max-w-full">
+            <h1 className="text-5xl md:text-6xl mt-40 md:mt-0 lg:text-7xl">  Living with regret is harder — it whispers in the quiet, long after the storm is gone.</h1>
+            <div className="flex flex-col mt-10 items-start gap-6 md:flex-row md:items-center">
+              <Button variant="secondary" iconAfter={
+                <Image className="size-5" src={downImage} alt="chevron-double-down image" />
+              }>
+                <span>
+                  View My Work
+                </span>
+
+              </Button>
+              <Button variant="text">Let&apos;s talk</Button>
+            </div>
+          </div>
+        </div>
+        <div className="md:col-span-5">
+          <div className="mt-20 md:mt-0 md:h-full">
+            <Image src={heroImage} alt="hero image" className="size-full object-cover" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+  )
 };
 
 export default Hero;
