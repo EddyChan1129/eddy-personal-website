8b3e248f75c32013b60c2b4b56ed82a51bbfb1fb
EddyChan1129 <22026266d@connect.polyu.hk>
2025-05-08 03:16:12 -0400

    Feat: Intro & project anitmation

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,9 +1,39 @@
+"use client";
+import { stagger } from "motion";
+import { useAnimate, useInView } from "motion/react";
 import { section } from "motion/react-client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
+
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, { once: true });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }
+  , [scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(scope.current.querySelectorAll(".word"),{
+        transform: "translateY(0)",
+      }, {
+        duration: 0.5,
+        delay: stagger(0.1),
+      }
+      );
+  
+    }
+  }, [inView, animate, scope]);
+
+
   return (
-    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro">
+    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro" ref={scope}>
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:w-[80%]">
           I’m Eddy Chan, a Product Designer based in Hong Kong. I specialize in creating user-centered designs that are not only visually appealing but also highly functional.
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -39,8 +39,9 @@ const Projects: FC = () => {
         <div className="mt-10 md:mt-16 lg:mt-20">
           {
             projects.map(({ name, image }) => (
-              <a href="#" key={name} className="border-t last:border-b border-stone-400  border-dotted py-6 md:py-8 lg:py-10 flex flex-col">
-                <div>
+              <a href="#" key={name} className="border-t last:border-b border-stone-400  border-dotted py-6 md:py-8 lg:py-10 flex flex-col relative group/project">
+                <div className="absolute bottom-0 left-0 w-full h-0 group-hover/project:h-full transition-all duration-500 bg-stone-300"></div>
+                <div className="relative">
                   <div className="aspect-video md:hidden">
                     <Image
                       src={image}
@@ -48,11 +49,31 @@ const Projects: FC = () => {
                       className="size-full object-cover"
                     />
                   </div>
-                  <div className="mt-8 md:mt-0 flex items-center justify-between">
-                    <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
+                  <div className="mt-8 md:mt-0 flex items-center justify-between md:grid md:[grid-template-columns:1fr_300px_max-content] md:gap-8">
+                    <div className="lg:group-hover/project:pl-8 transition-all duration-500">
+                      <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+                    </div>
+                    <div className="relative">
+                      <div className="absolute aspect-video w-full top-1/2 -translate-y-1/2 opacity-0 scale-90 group-hover/project:opacity-100 group-hover/project:scale-100 transition-all duration-500">
+                        <Image
+                          src={image}
+                          alt={`${name} image`}
+                          className="size-full object-cover rounded-lg"
+                        />
+                      </div>
+                    </div>
+                    <div className="lg:group-hover/project:pr-8 transition-all duration-500">
+                      <div className="size-6 overflow-hidden">
+                        <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-transform duration-500">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
 
 
                   </div>
