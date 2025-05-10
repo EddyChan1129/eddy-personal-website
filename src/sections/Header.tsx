9e3e052bf8620bbed452030dff7838dee609fdd0
EddyChan1129 <22026266d@connect.polyu.hk>
2025-05-09 22:30:47 -0400

    test

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -61,7 +61,7 @@ const Header: FC = () => {
       navAnimate(
         navScope.current,
         {
-          height: "100vh",
+          height: "110dvh",
         },
         {
           duration: 0.7,
@@ -126,7 +126,7 @@ const Header: FC = () => {
   return (
     <header>
       <div
-        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-200 z-10"
+        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900 z-10"
         ref={navScope}
       >
         <nav className="mt-20 flex flex-col">
@@ -134,7 +134,7 @@ const Header: FC = () => {
             <a
               href={href}
               key={label}
-              className="text-stone-900 border-t border-stone-800 last:border-b py-8 group/nav-item relative isolate"
+              className="text-stone-200 border-t border-stone-800 last:border-b py-8 group/nav-item relative isolate"
               onClick={handleClickMobileItem}
             >
               <div className="container max-w-full flex items-center justify-between">
