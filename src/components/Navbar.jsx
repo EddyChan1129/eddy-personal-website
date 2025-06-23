dfdc772483afc6b4b12f852128decdbffbde7608
EddyChan1129 <22026266d@connect.polyu.hk>
2025-06-22 20:56:27 -0400

    change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             {" "}
-            Eddie<span className="text-blue-500">.PROFILE</span>{" "}
+            Eddy<span className="text-blue-500">.PROFILE</span>{" "}
           </a>
 
           <div
